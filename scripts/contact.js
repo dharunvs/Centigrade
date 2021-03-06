a9db3d6bcf471bf20891328e64b6eddbe1607e2b
Dharundds <76607616+Dharundds@users.noreply.github.com>
2021-03-06 14:14:27 +0530

    Update contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,13 +1,38 @@
 function SubForm (){
     $.ajax({
-        url:"https://api.apispreadsheets.com/data/9062/",
+        url:"https://api.apispreadsheets.com/data/9087/",
         type:"post",
         data:$("#myForm").serializeArray(),
         success: function(){
-            alert("Form Data Submitted :)")
+                let x = test();
+                return x;          
         },
         error: function(){
             alert("There was an error :(")
         }
     });
 }
+function test(){
+   var a = document.forms["myForm"]["Full Name"].value;
+   var b = document.forms["myForm"]["Email"].value;                      
+   var c = document.forms["myForm"]["Phone No"].value;
+   var d = document.forms["myForm"]["Message"].value;
+  
+   if (a==" "||b==""||c==""||d==""){
+            alert("atleast one field you left blank");
+            return false;                       
+   }
+   else {
+        console.log("hi buddy")
+        var s = document.querySelector(".submitted");
+        s.classList.add("show");
+  
+        setTimeout(() => {
+            s.classList.remove("show");
+          }, 2000);
+                           return true;
+  
+                          
+    }
+ }
+  
